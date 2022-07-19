1d81064c6dcfc3cb2e213d3a0b2796370352590c
kai-oishi-git <kai.oishi.git@gmail.com>
2022-07-19 13:31:30 +0900

    観光地名で検索するメソッド追加

--- AccessDataBase/access_data_class.php
+++ AccessDataBase/access_data_class.php
@@ -3,9 +3,24 @@
 データベースと直接やり取りするクラス
 検索時はこのクラスのメソッドを使って情報を取得する
 */
+$mysql = new mysqli('localhost', "OkinawaGo", "OkinawaGo", "OkinawaGo");
 
 class AccessForDataBase {
-    public $mysql = new mysqli('localhost', "OkinawaGo", "OkinawaGo", "OkinawaGo");
 
+    static public function selectByName($name){
+        $mysqli = new mysqli('localhost', "OkinawaGo", "OkinawaGo", "OkinawaGo");
+
+        if (mysqli_connect_error()){
+            die("データベースの接続に失敗しました");
+            return;
+        }
+
+        $query = "";
+
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+    
+        $mysqli->close();
+    }
 }
 ?>
