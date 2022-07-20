15e0c78f6f935ce41ed82a254e8e33a50c9717ef
kyouhei-oda-git <kyouhei.oda.git@gmail.com>
2022-07-20 16:02:51 +0900

    AreaDTOの修正

--- Entity/Area.php
+++ Entity/Area.php
@@ -3,7 +3,7 @@ class Area {
     public $id_area;
     public $name_area;
 
-    public __construct($id_area, $name_area) {
+    function __construct($id_area, $name_area) {
         $this->$id_area = $id_area;
         $this->$name_area = $name_area;
     }
