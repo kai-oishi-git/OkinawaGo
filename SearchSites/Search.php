1cfbbcbef3e87598563d6c2170610fc440f9f84b
kai-oishi-git <kai.oishi.git@gmail.com>
2022-07-22 15:32:59 +0900

    エラー解消

--- SearchSites/Search.php
+++ SearchSites/Search.php
@@ -7,19 +7,19 @@ class Search {
         //AccessForDataBase->selectByNameで $inputSiteNameを引数として検索し、
         $sites = array();
         $areas = array();
-        array($sites, $areas) = AccessForDataBase->selectByName($inputSiteName);
+        list($sites, $areas) = AccessForDataBase::selectByName($inputSiteName);
         //結果をsiteインスタンス配列とareaインスタンス配列を取得
         
         // CombinedSiteのインスタンス配列を作る
         $combinedSites = array();
         //siteインスタンス配列の要素文ループするfor文を書く
-        for(int $i = 0; $i>count($sites); $i++){
+        for($i = 0; $i>count($sites); $i++){
         // for文内 : siteインスタンスのid_siteでimage,comment,categoryを検索し、インスタンス配列を受け取る
-            $comment = AccessForDataBase_toGetMultipleData->selectCommentBySiteID($site[$i]->$id_site);
-            $image = AccessForDataBase_toGetMultipleData->selectImageBySiteID($site[$i]->$id_site);
-            $category = AccessForDataBase_toGetMultipleData->selectCategoryBySiteID($site[$i]->$id_site);
+            $comment = AccessForDataBase_toGetMultipleData::selectCommentBySiteID($sites[$i]->id_site);
+            $image = AccessForDataBase_toGetMultipleData::selectImageBySiteID($sites[$i]->id_site);
+            $category = AccessForDataBase_toGetMultipleData::selectCategoryBySiteID($sites[$i]->id_site);
         // for文内 : site,area,image,comment,cartegoryでCombinedSiteインスタンスを作り、配列に格納
-            $combinedSite = new CombinedSite($site[$i], $area[$i], $comment, $image, $category);
+            $combinedSite = new CombinedSite($sites[$i], $areas[$i], $comment, $image, $category);
             array_push($combinedSites, $combinedSite);
         }
         // return CombinedSiteのインスタンス配列
