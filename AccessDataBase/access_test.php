b6e59e713dbe39477c026b26fe35dcc72b03f1a6
kai-oishi-git <kai.oishi.git@gmail.com>
2022-07-25 16:25:43 +0900

    AccessData追加

--- AccessDataBase/access_test.php
+++ AccessDataBase/access_test.php
@@ -1,15 +1,19 @@
 <?php
-require_once "access_data_multiple_class.php";
+require_once "access_data.php";
 
 
 //インスタンスメソッドのテスト
-$a = array();
-$b = new AccessForDataBase_toGetMultipleData();
-$a = $b->selectCommentBySiteID(0);
-print_r($a[1]);
+// $a = array();
+// $b = new AccessForDataBase_toGetMultipleData();
+// $a = $b->selectCommentBySiteID(0);
+// print_r($a[1]);
 
-//クラスメソッドのテスト
-$c = array();
-$c = AccessForDataBase_toGetMultipleData::selectCommentBySiteID(0);
-print_r($c[0]);
+// //クラスメソッドのテスト
+// $c = array();
+// $c = AccessForDataBase_toGetMultipleData::selectCommentBySiteID(0);
+// print_r($c[0]);
+
+$comment = AccessData::selectByFreeword("テスト");
+var_dump($comment);
+
 ?>
--- Entity/Site.php
+++ Entity/Site.php
@@ -2,13 +2,11 @@
 class Site {
     public $id_site;
     public $name_site;
-    public $id_area;
     public $address;
 
-    function __construct($id_site, $name, $id_area, $address) {
+    function __construct($id_site, $name, $address) {
         $this->id_site = $id_site;
         $this->name_site = $name;
-        $this->id_area = $id_area;
         $this->address = $address;
     }
 }
